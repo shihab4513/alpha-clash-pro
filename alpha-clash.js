f01fed57c8b91ba8526443b377377f740cd99f41
shihab4513 <shihab4513@gmail.com>
2024-02-26 18:28:39 +0600

    Build GetRandom Alphabet Function

--- alpha-clash.js
+++ alpha-clash.js
@@ -1,7 +1,15 @@
+function continueGame() {
+    const alphabet = getARandomAlphabet();
+}
+
+
 // Step-1 : hide the home screen. to hide the screen add the class hidden to the home section
 function play() {
     hideElementById('home');
 
     // show the playground
     showElementById('play-ground');
+
+    continueGame();
 }
+
--- utility.js
+++ utility.js
@@ -8,3 +8,13 @@ function showElementById(element) {
     const show = document.getElementById(element);
     show.classList.remove('hidden');
 }
+function getARandomAlphabet(){
+    // get or create an alphabet array
+    const alphabetString='abcdefghijklmnopqrstuvwxyz'
+    const alphabets=alphabetString.split('');
+
+    // get a random index from 0-25
+    const randomNumber=Math.random()*25;
+    const index=Math.round(randomNumber);
+    return alphabets[index];
+}
